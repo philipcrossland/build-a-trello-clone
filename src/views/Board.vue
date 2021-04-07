406d3a6a11c271be26fa772fe16f3ad2bccac613
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-07 11:16:44 +0100

    End of L4 - Adding and editing tasks

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -22,6 +22,13 @@
               {{ task.description }}
             </p>
           </div>
+
+          <input
+            type="text"
+            class="block p-2 w-full bg-transparent"
+            placeholder="+ Enter new task"
+            @keyup.enter="createTask($event, column.tasks)"
+          >
         </div>
       </div>
     </div>
@@ -48,6 +55,14 @@ export default {
     },
     close () {
       this.$router.push({ name: 'board' })
+    },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', {
+        tasks,
+        name: e.target.value
+      })
+
+      e.target.value = ''
     }
   }
 }
